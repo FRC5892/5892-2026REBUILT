487d38d9ce97245d06484485ab1965b9c4ab659b
Michael <117385299+mstatistics@users.noreply.github.com>
2026-03-09 10:49:23 -0500

    Update at 'Mon Mar 09 10:49:22 CDT 2026'

--- src/main/java/frc/robot/subsystems/shooter/Hood.java
+++ src/main/java/frc/robot/subsystems/shooter/Hood.java
@@ -27,7 +27,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.RobotState;
 import frc.robot.util.FieldConstants.LinesHorizontal;
 import frc.robot.util.FieldConstants.LinesVertical;
