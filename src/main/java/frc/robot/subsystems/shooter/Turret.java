7ea034bd2147f47693a7baefc5c8554a16489723
Michael <117385299+mstatistics@users.noreply.github.com>
2026-03-17 10:53:22 -0500

    revert unrealistic turret changes

--- src/main/java/frc/robot/subsystems/shooter/Turret.java
+++ src/main/java/frc/robot/subsystems/shooter/Turret.java
@@ -54,9 +54,9 @@ public class Turret extends SubsystemBase {
 
   /* Movement Constants */
   private final LoggedTunableMeasure<MutAngle> minAngle =
-      new LoggedTunableMeasure<MutAngle>("Turret/MinAngle", Degrees.mutable(-210));
+      new LoggedTunableMeasure<MutAngle>("Turret/MinAngle", Degrees.mutable(-202));
   private final LoggedTunableMeasure<MutAngle> maxAngle =
-      new LoggedTunableMeasure<MutAngle>("Turret/MaxAngle", Degrees.mutable(210));
+      new LoggedTunableMeasure<MutAngle>("Turret/MaxAngle", Degrees.mutable(205));
   /* Homing */
   private final LoggedTunableNumber homingVoltage =
       new LoggedTunableNumber("Turret/Homing/Voltage", 4, "v");
