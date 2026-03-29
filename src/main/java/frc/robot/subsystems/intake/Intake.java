a96f4221c7de233391564955f667de4a9f249fb2
Michael <117385299+mstatistics@users.noreply.github.com>
2026-03-29 09:24:08 -0500

    Update at 'Sun Mar 29 09:24:08 CDT 2026'

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -62,9 +62,8 @@ public class Intake extends SubsystemBase {
         () ->
             rollerMotor.setControl(
                 dutyCycleOut.withOutput(
-                    rollerMotor.getPrimaryTorqueCurrentAmps() > unjamThreshold.get()
-                        ? unjamSpeed.get()
-                        : rollerSpeed.get())),
+                    // rollerMotor.getPrimaryTorqueCurrentAmps() > unjamThreshold.get() ? unjamSpeed.get() :
+                      rollerSpeed.get())),
         () -> rollerMotor.setControl(dutyCycleOut.withOutput(0)));
   }
 
