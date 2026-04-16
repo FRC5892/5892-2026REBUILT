105173a1876f04f75e8c1e92c0431edc1808c7ea
Michael <117385299+mstatistics@users.noreply.github.com>
2026-04-16 10:06:43 -0500

    format

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -49,16 +49,16 @@ public class Autos {
                   ShootCommands.shoot(indexer, shooter, Goal.HUB),
                   Commands.waitSeconds(7),
                   Commands.sequence(Commands.waitSeconds(3.5), intake.hold())),
-                  Commands.race(
-                      Commands.sequence(
-                          AutoBuilder.followPath(loadPath("Left_Path", points)),
-                          AutoBuilder.followPath(loadPath("Left_Path_Return", points))),
-                      intake.intakeSequence(),
-                      shooter.getHood().stowCommand()),
-                  Commands.race(
-                      ShootCommands.shoot(indexer, shooter, Goal.HUB),
-                      Commands.waitSeconds(7),
-                      Commands.sequence(Commands.waitSeconds(3.5), intake.hold())));
+              Commands.race(
+                  Commands.sequence(
+                      AutoBuilder.followPath(loadPath("Left_Path", points)),
+                      AutoBuilder.followPath(loadPath("Left_Path_Return", points))),
+                  intake.intakeSequence(),
+                  shooter.getHood().stowCommand()),
+              Commands.race(
+                  ShootCommands.shoot(indexer, shooter, Goal.HUB),
+                  Commands.waitSeconds(7),
+                  Commands.sequence(Commands.waitSeconds(3.5), intake.hold())));
       // More boilerplate
       if (Constants.currentMode == Constants.Mode.SIM) {
         Logger.recordOutput(
